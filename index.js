68a3f18f38fa00fea39580a59d778078ae87f833
Manuel Avila <magolitonft2311@gmail.com>
2023-04-12 16:18:29 -0400

    90% complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const matchingDrivers = drivers.filter(function(driver) {
+      return driver.toLowerCase() === name.toLowerCase();
+    });
+  
+    return matchingDrivers;
+  }
+
+function fuzzyMatch(drivers, letters) {
+    const matchingDrivers = drivers.filter(function(driver) {
+    return driver.slice(0, letters.lenght) === letters;
+});
+return matchingDrivers;
+
+}
+
+function matchName(drivers, name) {
+    const matchingDrivers = drivers.filter(function(driver) {
+      return driver.name === name;
+    });
+  
+return matchingDrivers;
+  }
+
+
